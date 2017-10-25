8692ab6ec4eb32e353baf2d305277a7ba6144e93
Deepak54321 <31242754+Deepak54321@users.noreply.github.com>
2017-10-25 08:06:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ def webhook():
     print(json.dumps(req, indent=4))
 
     res = processRequest(req)
-
     res = json.dumps(res, indent=4)
     # print(res)
     r = make_response(res)
@@ -50,55 +49,19 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "Demoapi":
-        return {}
-    speech="110000";
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
-
-
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
-
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
+	if req.get("result").get("action") =="Demoapi"
+		#baseurl = "http://www.yamaha-motor-india.com/iym-web-api//51DCDFC2A2BC9/statewiseprice/getprice?product_profile_id=salutorxspcol&state_id=240"
+		#yql_url = baseurl 
+		#result = urlopen(yql_url).read()
+		#data = json.loads(result)
+        data = "14"
+		res = makeWebhookResult(data)
+		return res
 
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
+    
 
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Price is " data 
 
     print("Response:")
     print(speech)
