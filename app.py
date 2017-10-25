6bdabfcedc6a695cfa9b865fb707ec5a7db2ba7c
Deepak54321 <31242754+Deepak54321@users.noreply.github.com>
2017-10-25 08:22:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def webhook():
     print(json.dumps(req, indent=4))
 
     res = processRequest(req)
+
     res = json.dumps(res, indent=4)
     # print(res)
     r = make_response(res)
@@ -49,26 +50,55 @@ def webhook():
 
 
 def processRequest(req):
-	if req.get("result").get("action") !="Priceapi":
-		return {}
-		#baseurl = "http://www.yamaha-motor-india.com/iym-web-api//51DCDFC2A2BC9/statewiseprice/getprice?product_profile_id=salutorxspcol&state_id=240"
-		#full_url = baseurl  
-		#result = urlopen(full_url).read()
-		#data = json.loads(result)
-		data = "14"
-		#res = makeWebhookResult(data)
-		#return res
-		
-		return {
-        		"speech": data,
-        		"displayText": data,
-       			# "data": data,
-        		# "contextOut": [],
-        		"source": "apiai-weather-webhook-sample"
-    			}
-		
+    if req.get("result").get("action") != "Demoapi":
+        return {}
+    speech="Hello World";
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
 def makeWebhookResult(data):
-    speech = "Current Pirce: " + data
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
@@ -79,7 +109,7 @@ def makeWebhookResult(data):
         # "data": data,
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
-    	}
+    }
 
 
 if __name__ == '__main__':
