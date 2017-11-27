259c1e0da5408c882e25838aebd897b95c2bea87
Deepak54321 <dmishra@yamaha-motor-india.com>
2017-11-27 01:14:37 -0800

    IC

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding:utf8 -*-
 # !/usr/bin/env python
-# Copyright 2017 Google Inc. All Rights Reserved.
+# Copyright 2017 Google Inc.  All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from __future__ import print_function
 from future.standard_library import install_aliases
 install_aliases()
@@ -49,7 +48,7 @@ def webhook():
 
 
 def processRequest(req):
-	if req.get("result").get("action") =="Priceapi":
+	if req.get("result").get("action") == "Priceapi":
 		baseurl = "http://www.yamaha-motor-india.com/iym-web-api//51DCDFC2A2BC9/statewiseprice/getprice?product_profile_id=salutorxspcol&state_id=240"
 		full_url = baseurl  
 		result = urlopen(full_url).read()
@@ -65,42 +64,40 @@ def processRequest(req):
         		# "contextOut": [],
         		"source": "apiai-weather-webhook-sample"
     			}
-	if req.get("result").get("action") =="Dealerapi":
+	if req.get("result").get("action") == "Dealerapi":
 		result = req.get("result")
 		parameters = result.get("parameters")
-		state=parameters.get('State')
-		city=parameters.get('geo-city')
+		state = parameters.get('State')
+		city = parameters.get('geo-city')
 		baseurl = "http://www.yamaha-motor-india.com/iym-web-api//51DCDFC2A2BC9/network/search?type=sales&profile_id=gujarat&city_profile_id=ahmedabad"
 		full_url = baseurl  
 		result = urlopen(full_url).read()
 		data = json.loads(result)
 		responseData = data.get('responseData')
-		speech=""
+		speech = ""
 		dealers = responseData.get('dealers')
 		if dealers is None:
-			speech="No Dealer Found in your city please check the city you entered"		
+			speech = "No Dealer Found in your city please check the city you entered"		
 		for i in range(len(dealers)):
 			dealername = dealers[i]['dealer_name']
-			dealeraddress=dealers[i]['dealer_address']
-			dealersalmgrmob=dealers[i]['sales_manager_mobile']
-			speech+='Dealer name :' + dealername + '\n'  + 'Dealer Address :' + dealeraddress + '\n'  + 'Dealer Salese Manager Mobile No :' + dealersalmgrmob + '\n' + '\n' 
+			dealeraddress = dealers[i]['dealer_address']
+			dealersalmgrmob = dealers[i]['sales_manager_mobile']
+			speech+='Dealer name :' + dealername + '\n' + 'Dealer Address :' + dealeraddress + '\n' + 'Dealer Salese Manager Mobile No :' + dealersalmgrmob + '\n' + '\n' 
 		return {
 			"speech":speech,
 			"displayText":speech,
 			}
-	if req.get("result").get("action") =="intro":
+	if req.get("result").get("action") == "intro":
 	    return {
 		      'speech': 'When',
               'displayText': 'When',
               'messages': 
-              [
-               {'title': 'Please choose one of the following options',
+              [{'title': 'Please choose one of the following options',
                 'replies': ['Product Enquiry',
                             'Test Drive',
                             'Complaints',
                             'Yamaha News'],
-                'type': 2}
-              ],
+                'type': 2}],
               'source': 'dimwei.com'
 		}
 	
